2819e44e5962297a07a60f5c1d086915098a75be
ab.smirnov <ab.smirnov@stalcom.com>
2023-09-13 23:52:56 +0300

    Добавлена абстракция пылесоса и рандомно ездящий пылесос

---
+++ VacuumCleaner/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VacuumCleaner;
+using VacuumCleaner.RandomRidingCleaner;
+
+//Инициализация комнаты для уборки - размер и препятствия.
+//Препятствия - единички на схеме.
+//0 0 0 0 0 0 0
+//0 0 0 0 0 0 0
+//0 0 1 0 1 1 0
+//0 0 0 0 1 1 0
+//0 0 0 0 0 0 0
+//0 1 0 0 0 0 0
+//0 1 0 0 0 0 0
+//1 1 0 0 0 0 0
+var room = new Room(8, 7);
+room.SetBarrier(2, 5);
+room.SetBarrier(2, 4);
+room.SetBarrier(3, 5);
+room.SetBarrier(3, 4);
+room.SetBarrier(2, 2);
+room.SetBarrier(5, 1);
+room.SetBarrier(6, 1);
+room.SetBarrier(7, 1);
+room.SetBarrier(7, 0);
+//room.CleanArea(1,0, 2, 1); 
+
+var cleaner = new RandomRidingCleaner(2, 3, room);
+cleaner.Clean();
+
+//Отобразить поле в процессе работы
+// while (!cleaner.HasFinished())
+// {
+// 	for(var i=0; i<room.Length; i++)
+// 	{
+// 		for (int j = 0; j < room.Width; j++)
+// 		{
+// 			Console.Write(room.Space[i, j].ToString() + ' ');
+// 		}
+// 		Console.WriteLine();
+// 	}
+// 	Console.WriteLine();
+// 	Thread.Sleep(500);  //TODO тормозит все выполнение, но пока для примера пойдет. сделать выполнение пылесоса в отдельном потоке.
+// }
+
+for(var i=0; i<room.Length; i++)
+{
+	for (int j = 0; j < room.Width; j++)
+	{
+		Console.Write(room.Space[i, j].ToString() + ' ');
+	}
+	Console.WriteLine();
+}
+Console.WriteLine($"Очистка завершена. Очищено {room.CalculateCleanPercent()}% комнаты.");
+
+
+
---
+++ VacuumCleaner/Room.cs
@@ -0,0 +1,95 @@
+namespace VacuumCleaner;
+
+/// <summary>
+/// Представляет комнату.
+/// </summary>
+public class Room : IRoom
+{
+	/// <summary>
+	/// Инициализирует экземпляр типа <see cref="Room"/>.
+	/// </summary>
+	/// <param name="length">Длина комнаты.</param>
+	/// <param name="width">Ширина комнаты.</param>
+	public Room(int length, int width)
+	{
+		Space = new int[length, width];
+		Length = length;
+		Width = width;
+	}
+	
+	/// <inheritdoc/>
+	public bool DoesCellExist(int xCoordinate, int yCoordinate)
+	{
+		try
+		{
+			return Space[xCoordinate, yCoordinate] != 1;
+		}
+		catch (IndexOutOfRangeException e)
+		{
+			return false;
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+			throw;
+		}
+		
+	}
+
+	/// <inheritdoc/>
+	public void SetBarrier(int xCoordinate, int yCoordinate)
+	{
+		Space[xCoordinate, yCoordinate] = 1;
+	}
+
+	/// <inheritdoc/>
+	public int CalculateCleanPercent()
+	{
+		var cellCount = Length * Width;
+		var cleanCells = 0;
+
+		for(var i=0; i<Length; i++)
+		{
+			for (int j = 0; j < Width; j++)
+			{
+				if (Space[i, j] == 2)
+				{
+					cleanCells++;
+				}
+			}
+		}
+
+		return cleanCells * 100 / cellCount;
+	}
+
+	/// <inheritdoc/>
+	public void CleanArea(int xStart, int yStart, int xFinish, int yFinish)
+	{
+		for (var i = xStart; i <= xFinish; i++)
+		{
+			for (var j = yStart; j <= yFinish; j++)
+			{
+				Space[i, j] = 2;
+			}
+		}
+		
+	}
+
+	public int[,] Space
+	{
+		get;
+		private set;
+	}
+
+	public int Length
+	{
+		get;
+		private set;
+	}
+	
+	public int Width
+	{
+		get;
+		private set;
+	}
+}
